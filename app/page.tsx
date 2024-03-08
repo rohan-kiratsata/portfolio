84723c65fff2aa5c32a25f4867ce06b7f2ae57bb
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2024-03-08 19:51:48 +0530

    onboarding removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Onboarding from "@/components/home/Onboarding";
-
 export default function Home() {
   return (
     <>
@@ -7,14 +5,7 @@ export default function Home() {
         <p className="text-7xl font-medium">Hi, I&apos;m Rohan Kiratsata</p>
 
         {/* Overview */}
-        <div className="my-10">
-          {/* <p className="text-base text-content1 w-[70%] mt-5">
-            Full Stack Developer passionate about building products, Indie
-            Hacker and full-time freelancer based in India. Currently building a
-            chrome extension for devs and designers @velocity-tools
-          </p> */}
-          <Onboarding />
-        </div>
+        <div className="my-10"></div>
       </div>
     </>
   );
