aca958363bc618422dcea4501b6236de3f1b0a7d
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2024-03-22 14:43:16 +0530

    changes

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,20 +2,43 @@
 
 import React, { useEffect, useState } from "react";
 import { navbarItems } from "@/utils/content";
-import { NavbarItemCardProps } from "@/types";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Modal,
+  ModalContent,
+  ModalBody,
+  useDisclosure,
+  Button,
+  Divider,
+  Tooltip,
+  ModalHeader,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { Kbd } from "@nextui-org/kbd";
 import generateID from "@/utils/generateId";
-import useSound from "use-sound";
-import Link from "next/link";
-import ContactModal from "./ContactModal";
+// import CrypticEffect from "../CrypticEffect";
+import ContactSocials from "./ContactSocials";
+import { PiNotePencil, PiX } from "@/utils/iconImports";
+import CopyToClipboard from "../CopyToClipboard";
+import Toast from "../Toast";
 
 type Props = {};
 
-export default function Navbar({}: Props) {
-  const router = useRouter();
+export default function TopNavbar({}: Props) {
+  // Hook to manage navbar mene state
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // Custom hooks to manage modal open
+  const { onOpen, isOpen, onOpenChange } = useDisclosure();
 
+  const router = useRouter();
+
+  // Mapping keys
   const keyToUrlMap = navbarItems.reduce((acc: any, item: any) => {
     if (item.kbd) {
       acc[item.kbd] = item.link;
@@ -24,6 +47,7 @@ export default function Navbar({}: Props) {
   }, {});
 
   useEffect(() => {
+    // This function listens to key presses on keyboard and redirects to the page
     const handleKeyPress = (e: any) => {
       if (e.key === "/") {
         e.preventDefault();
@@ -41,110 +65,144 @@ export default function Navbar({}: Props) {
 
   return (
     <>
-      {/* border border-content1/30  */}
-      <aside className="my-10 bg-background relative">
-        <div className="flex flex-row items-center justify-between">
-          <header className="">
-            <Link href={"/"} className="">
-              <div className="w-10 h-10 bg-white flex items-center justify-center rounded-full">
-                <h1 className="text-black text-lg font-monorounded-full">R</h1>
-              </div>
-            </Link>
-          </header>
+      <Navbar
+        className="my-10"
+        onMenuOpenChange={setIsMenuOpen}
+        position="static"
+      >
+        <NavbarBrand>
+          <Link href={"/"}>
+            <div className="w-10 h-10 bg-white flex items-center justify-center rounded-full">
+              <h1 className="text-black text-lg font-monorounded-full">R</h1>
+            </div>
+          </Link>
+        </NavbarBrand>
+        <NavbarContent justify="end">
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          {navbarItems.map((item) => {
+            if (item.title === "Contact") {
+              return (
+                <>
+                  <NavbarItem
+                    key={generateID("NAVBAR")}
+                    className="hidden lg:flex"
+                  >
+                    <span
+                      className="w-full text-white hover:underline font-mono tracking-wide cursor-pointer"
+                      onClick={onOpen}
+                    >
+                      {item.title.toUpperCase()}
+                    </span>
+                  </NavbarItem>
+                  <Modal
+                    isOpen={isOpen}
+                    onOpenChange={onOpenChange}
+                    size="2xl"
+                    hideCloseButton
+                  >
+                    <ModalContent>
+                      {(onClose) => (
+                        <>
+                          <div className="bg-neutral-800 py-5 px-5 rounded-lg ">
+                            {/* Header */}
+                            <ModalHeader className="flex justify-between px-0 pb-6 pt-1">
+                              <h2 className="text-xl font-sans font-medium">
+                                Contact
+                              </h2>
+                              <div
+                                onClick={onClose}
+                                className="hover:bg-neutral-700 p-1 rounded-lg cursor-pointer"
+                              >
+                                <PiX size={18} />
+                              </div>
+                            </ModalHeader>
+                            <div className="">
+                              {/* Email */}
+                              <span className="text-lg">Email</span>
+                              <div className="flex justify-between items-center">
+                                <Tooltip
+                                  content="Copy email"
+                                  placement="bottom"
+                                  key="cpy-email"
+                                  delay={100}
+                                  closeDelay={100}
+                                  className="rounded-md"
+                                >
+                                  <div className="p-2 hover:bg-neutral-700 w-fit rounded-lg duration-100 text-content1 hover:text-white cursor-pointer">
+                                    <CopyToClipboard content="kiratsatarohan@gmail.com">
+                                      <Toast content="Copied to clipboard">
+                                        <span>kiratsatarohan@gmail.com</span>
+                                      </Toast>
+                                    </CopyToClipboard>
+                                  </div>
+                                </Tooltip>
+                                {/* Componse button */}
+                                <Button
+                                  size="sm"
+                                  variant="bordered"
+                                  className="border-1"
+                                  as={Link}
+                                  href="mailto:kiratsatarohan@gmail.com"
+                                >
+                                  <PiNotePencil size={18} /> Compose
+                                </Button>
+                              </div>
 
-          {/* Hamburger menu */}
-          <button
-            className="block md:hidden"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            ☰
-          </button>
+                              <Divider className="my-5 opacity-30" />
 
-          {/* Navbar */}
-          <nav
-            className={`md:flex flex-row  gap-4 ${
-              isMenuOpen
-                ? "absolute right-0 top-full bg-neutral-800 p-2 rounded-md flex flex-col gap-3"
-                : "hidden"
-            }`}
-          >
-            {navbarItems.map((item) => {
+                              {/* Socials */}
+                              <span className="text-lg">Around the web</span>
+                              <div className="flex flex-row justify-between items-center">
+                                <p className="text-content1">
+                                  I&apos;m mostly active on Twitter.
+                                </p>
+                                <div>
+                                  <ContactSocials />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      )}
+                    </ModalContent>
+                  </Modal>
+                </>
+              );
+            } else
               return (
                 <NavbarItem
-                  key={generateID("NAV")}
-                  link={item.link}
-                  title={item.title}
-                  kbd={item.kbd}
-                  caption={item.caption}
-                />
+                  key={generateID("NAVBAR")}
+                  className="hidden lg:flex"
+                >
+                  <Link
+                    className="w-full font-mono tracking-wide"
+                    href={item.link}
+                    size="md"
+                    underline="hover"
+                  >
+                    {item.title.toUpperCase()}
+                  </Link>
+                </NavbarItem>
               );
-            })}
-          </nav>
-        </div>
-      </aside>
+          })}
+        </NavbarContent>
+        <NavbarMenu className="my-20">
+          {navbarItems.map((item) => (
+            <NavbarMenuItem key={generateID("NAVBAR-MENU")} className="my-1">
+              <Link
+                className="w-full font-mono text-3xl"
+                href={item.link}
+                size="lg"
+              >
+                {item.title.toUpperCase()}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
     </>
   );
 }
-
-const NavbarItem = ({ title, caption, link, kbd }: NavbarItemCardProps) => {
-  // Hooks to trigger modal
-  const [isModalOpen, setIsOpenModal] = useState(false);
-
-  const openModal = () => {
-    setIsOpenModal(true);
-  };
-  const closeModal = () => setIsOpenModal(false);
-
-  useEffect(() => {
-    const openModalOnKeyPress = (e: any) => {
-      if (e.key === "/") {
-        openModal();
-      } else if (e.key === "Escape") {
-        closeModal();
-      }
-    };
-    window.addEventListener("keydown", openModalOnKeyPress);
-    return () => {
-      window.removeEventListener("keydown", openModalOnKeyPress);
-    };
-  }, []);
-
-  const isExternalLink = (link: string) => {
-    return link.startsWith("http") || link.startsWith("//");
-  };
-
-  return (
-    <>
-      {title === "Contact" ? (
-        <>
-          <div
-            onClick={openModal}
-            className="hover:bg-neutral-800 rounded-md h-fit p-1 cursor-pointer inline-flex gap-2"
-          >
-            <h3>{title}</h3> <Kbd>{kbd}</Kbd>
-          </div>
-          <div className="w-full h-full overflow-x-hidden">
-            <ContactModal isOpen={isModalOpen} onClose={closeModal} />
-          </div>
-        </>
-      ) : (
-        <>
-          {isExternalLink(link) ? (
-            <div className="hover:bg-neutral-800 rounded-md h-fit p-1">
-              <a href={link} target="_blank" className="inline-flex gap-2">
-                <span>{title}</span> <Kbd>{kbd}</Kbd>
-              </a>
-            </div>
-          ) : (
-            <div className="hover:bg-neutral-800 rounded-md h-fit p-1">
-              <Link href={link} className="inline-flex gap-2">
-                <span>{title}</span>
-                <Kbd>{kbd}</Kbd>
-              </Link>
-            </div>
-          )}
-        </>
-      )}
-    </>
-  );
-};
