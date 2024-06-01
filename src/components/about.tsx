7b5fdf7e459aed1cd001df11ab93e0cceaf785c7
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2024-06-02 01:35:43 +0530

    minor bug fix

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,10 +1,11 @@
+import { site } from "@/config";
 import React from "react";
 
 export default function About() {
   return (
     <div>
       <h2 className="heading">about</h2>
-      <p className="text-base text-neutral-500"></p>
+      <p className="text-base text-neutral-500">{site.description}</p>
     </div>
   );
 }
