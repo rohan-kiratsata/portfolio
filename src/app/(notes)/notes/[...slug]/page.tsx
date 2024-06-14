63170531fde3578394ea469268e93f3e96d99347
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2024-06-14 15:44:51 +0530

    commented `generateStaticParams` for deployment fix

--- src/app/(notes)/notes/[...slug]/page.tsx
+++ src/app/(notes)/notes/[...slug]/page.tsx
@@ -11,22 +11,22 @@ const notesDirectory = path.join(process.cwd(), "notes");
 
 console.log("NOTES: ", notesDirectory);
 
-export async function generateStaticParams() {
-  const notes = readNotes();
+// export async function generateStaticParams() {
+//   const notes = readNotes();
 
-  return notes.map((note: any) => {
-    const relativePath = note.filePath.replace(/\\/g, "/");
-    console.log("RELATIVE PATH:", relativePath);
-    const slug = relativePath
-      .replace("/notes/", "")
-      .replace(".md", "")
-      .split("/");
-    console.log("SLUG:", slug);
-    return {
-      slug: slug,
-    };
-  });
-}
+//   return notes.map((note: any) => {
+//     const relativePath = note.filePath.replace(/\\/g, "/");
+//     console.log("RELATIVE PATH:", relativePath);
+//     const slug = relativePath
+//       .replace("/notes/", "")
+//       .replace(".md", "")
+//       .split("/");
+//     console.log("SLUG:", slug);
+//     return {
+//       slug: slug,
+//     };
+//   });
+// }
 
 export default async function NotePage({ params }: { params: any }) {
   const router = useRouter();
