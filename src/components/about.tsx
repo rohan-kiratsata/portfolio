bb70d4ded0303070312b3ca665e320423be531be
rohanzig <49232485+rohan-kiratsata@users.noreply.github.com>
2024-09-23 10:24:37 +0530

    [HOTFIX]

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -8,12 +8,12 @@ export default function About() {
     <div>
       <h2 className="heading">about</h2>
       <p className="text-base text-neutral-500">{about}</p>
-      <Link href="https://notes.rohankiratsata.xyz" className="font-mono mt-1 text-neutral-100 hover:text-green-400">
+      <Link href="https://notes.rohankiratsata.xyz" className="font-mono mt-2 tracking-tight text-neutral-100 hover:text-green-400">
       tech notes/blogs
       </Link>
       <p className="text-neutral-100 mt-1">
-        open to new opportunitie //{" "}
-        <Link href="mailto:kiratsatarohan@gmail.com">
+        open to new opportunities //{" "}
+        <Link href="mailto:kiratsatarohan@gmail.com hover:text-green-400">
           kiratsatarohan@gmail.com
         </Link>
       </p>
